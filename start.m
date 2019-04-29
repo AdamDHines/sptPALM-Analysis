1a7d3083e39c2b9d87fc74b13a4255228164ff2e
AdamDHines <50095916+AdamDHines@users.noreply.github.com>
2019-04-29 16:46:09 +1000

    Initial commit of sptPALM as of 20190429

---
+++ start.m
@@ -0,0 +1,10 @@
+function start()
+
+% add the path for program code
+addpath('Analysis Code');
+addpath('GUI Code');
+addpath('File Search Code');
+addpath('Output Code');
+
+% launch sptPALM Analysis
+sptPALMParam;
